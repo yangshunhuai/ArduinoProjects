6f3535453617e2afcfc9c3838992655f37fc7b58
yangshunhuai <67225026+yangshunhuai@users.noreply.github.com>
2021-06-28 22:36:06 +0800

    CPUMemMonitor: Add client and server program

--- projects/CPUMemMonitor/MonitorClient/MonitorClient.ino
+++ projects/CPUMemMonitor/MonitorClient/MonitorClient.ino
@@ -3,19 +3,25 @@ LiquidCrystal_I2C scr(0x27, 16, 2);
 
 int percentCPU = 0;
 int percentMem = 0;
-int blockCPU = 0;
-int blockMem = 0;
 String serialData;
 char serialCharacter;
 //bool completeSerial = false;
-byte block[8] = {0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F};
+byte block1[8] = {0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10};
+byte block2[8] = {0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18};
+byte block3[8] = {0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C};
+byte block4[8] = {0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E};
+byte block5[8] = {0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   scr.init();
   scr.backlight();
-  scr.createChar(0, block);
+  scr.createChar(0, block1);
+  scr.createChar(1, block2);
+  scr.createChar(2, block3);
+  scr.createChar(3, block4);
+  scr.createChar(4, block5);
   //  scr.cursor();
   printBasic();
 }
@@ -28,11 +34,6 @@ void loop() {
   bool completeSerial = false;
   serialData = "";
   //printBasic();
-  printPercentCPU(percentCPU);
-  printPercentMem(percentMem);
-  printBlockMem(blockMem);
-  printBlockCPU(blockCPU);
-
   while (!completeSerial) {
     while (Serial.available()) {
       // get the new byte:
@@ -48,7 +49,12 @@ void loop() {
     }
   }
   splitStringAndToInt(serialData, ",");
-  translateIntoBlock(percentCPU, percentMem);
+  int blockCPU = translateCPUIntoBlock(percentCPU);
+  int blockMem = translateMemIntoBlock(percentMem);
+  printPercentCPU(percentCPU);
+  printPercentMem(percentMem);
+  printBlockMem(blockMem);
+  printBlockCPU(blockCPU);
 }
 
 void splitStringAndToInt(String inString, String separator) {
@@ -64,9 +70,14 @@ void splitStringAndToInt(String inString, String separator) {
   percentMem = str2.toInt();
 }
 
-void translateIntoBlock(int valueCPU, int valueMem) {
+int translateCPUIntoBlock(int valueCPU) {
   blockCPU = valueCPU / 15;
+  return blockCPU;
+}
+
+int translateMemIntoBlock(int valueMem) {
   blockMem = valueMem / 15;
+  return blockMem;
 }
 
 String readSerial() {
@@ -93,7 +104,7 @@ void printBlockCPU(int blockCPU) {
   scr.setCursor(5, 0);
   int i = 0;
   for (i; i < blockCPU; i++) {
-    scr.write(byte(0));
+    scr.write(byte(4));
   }
 }
 
@@ -102,19 +113,48 @@ void printBlockMem(int blockMem) {
   scr.setCursor(5, 1);
   int j = 0;
   for (j; j < blockMem; j++) {
-    scr.write(byte());
+    scr.write(byte(4));
   }
+}
+
+void processBlockCPU(int percentCPUPartial) {
+  int blockNum = translateCPUIntoBlock(percentCPUPartial);
+  
 }
 
 void printPercentCPU(int percentCPUPartial) {
   resetCPUPercent();
-  scr.setCursor(12, 0);
+  String percentCPUString = String(percentCPUPartial);
+  int digits = percentCPUString.length();
+  switch (digits) {
+    case 1 :
+      scr.setCursor(14, 0);
+      break;
+    case 2 :
+      scr.setCursor(13, 0);
+      break;
+    case 3 :
+      scr.setCursor(12, 0);
+      break;
+  }
   scr.print(percentCPUPartial);
 }
 
 void printPercentMem(int percentMemPartial) {
   resetMemPercent();
-  scr.setCursor(12, 1);
+  String percentMemString = String(percentMemPartial);
+  int digits = percentMemString.length();
+  switch (digits) {
+    case 1 :
+      scr.setCursor(14, 1);
+      break;
+    case 2 :
+      scr.setCursor(13, 1);
+      break;
+    case 3 :
+      scr.setCursor(12, 1);
+      break;
+  }
   scr.print(percentMemPartial);
 }
 
--- projects/CPUMemMonitor/MonitorServer/MonitorServer.py
+++ projects/CPUMemMonitor/MonitorServer/MonitorServer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import serial
+from serial.serialutil import SerialException
+import psutil
+import time
 
+refreshRate = 0
 # Baud rate
 baud = 9600
 # Port (Windows)
@@ -11,4 +15,25 @@ port = "COM9"
 # port = "/dev/ttyUSB0"
 
 # Create serial object
-ser = serial.Serial(port, baud)
+try:
+    ser = serial.Serial(port, baud)
+except SerialException:
+    print('Warning: Port not found! The client is not connected.')
+    exit()
+
+def getCPUPercent():
+    cpuPercent = psutil.cpu_percent(1)
+    print('CPU: ' + str(int(cpuPercent)))
+    return int(cpuPercent)
+
+def getMemPercent():
+    mem = psutil.virtual_memory()
+    memPercent = mem.percent
+    print('Mem: ' + str(int(memPercent)))
+    return int(memPercent)
+
+while True:
+    sendString = str(getCPUPercent()) + ',' + str(getMemPercent()) + '\n'
+    encodedString = sendString.encode('ascii')
+    ser.write(encodedString)
+    time.sleep(refreshRate)
